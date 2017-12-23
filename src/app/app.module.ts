1ec028f9f536e78cd06d55791ae2ed34ab93ee87
Byll Mcsony <bylljoseph@gmail.com>
2017-12-23 14:07:13 +0300

    эелементы дизайна

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 import { AppComponent } from './components/app/app.component';
 import { CommonComponent } from './components/common/common.component';
 import { AppRoutingModule } from './modules/app-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DomHelperService } from './services/dom/dom.helper.service';
 
 import {
   PanelModule,
@@ -24,7 +26,7 @@ import { ZkHeaderComponent } from './components/zk-header/zk-header.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DomHelperService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomHelperService } from '../../services/dom/dom.helper.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  publictitle = 'app';
+  public layoutWrapperHeight: string;
+  public bg= 'red';
+
+  constructor (private domHelperService: DomHelperService) {}
+  ngOnInit () {
+    this.layoutWrapperHeight = (100 - this.domHelperService.pxTovh(70)) + 'vh';
+    console.log(this.layoutWrapperHeight);
+  }
 }
