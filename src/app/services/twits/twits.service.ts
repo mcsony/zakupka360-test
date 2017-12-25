8bdf8d64143c35c3fd4ecb5b5bd154807a18942a
Byll Mcsony <bylljoseph@gmail.com>
2017-12-25 09:46:59 +0300

    сервис получения твитов

---
+++ src/app/services/twits/twits.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { AppSettings } from '../../../settings';
+import { Twit } from '../../models/twit';
+@Injectable()
+export class TwitsService {
+
+  constructor(private http: Http) { }
+  public static getHeaders() {
+    const headers = new Headers();
+    headers.append(AppSettings.getConfig()['DEFAULT_HEADER_KEY'], (AppSettings.getConfig()['HEADER_PREFIX'] +
+      AppSettings.getConfig()['DEFAULT_HEADER_VALUE']));
+    return new RequestOptions({headers: headers});
+  }
+  search(query): Promise<any> {
+   return this.http
+      .get(AppSettings.getConfig()['TWITS_URL'] + '?q=' + query +
+        '&count=' + AppSettings.getConfig()['TWITS_LIMIT'] ,
+        TwitsService.getHeaders())
+      .toPromise()
+      // .then(response => response.json() as Organization[])
+      .then(function (response) {
+        const res = response.json();
+        let data, decoded;
+        try {
+          decoded = JSON.parse(res.result) as Twit[];
+          data = decoded.statuses as Twit[];
+        } catch (err) {}
+
+        return data;
+      })
+      .catch((error) => { console.log(error); });
+
+  }
+}
